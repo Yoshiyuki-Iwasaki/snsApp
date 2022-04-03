87b6fd06ecb55b5ebf153bb063c1119dbc39df7a
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-04-03 22:08:32 +0900

    fix routes file.

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: redirect('/todos')
+  root to: redirect('/todos') # todosにリダイレクトさせる
 
   get 'todos', to: 'site#index'
   get 'todos/new', to: 'site#index'
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      delete '/todos/destory_all', to: 'todo#destory_all'
+      delete '/todos/destory_all', to: 'todos#destory_all'
       resources :todos, only: %i[index show create update destory]
     end
   end
