52b905defbf62df67de4e51b87eee711dfeeaaaf
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-04-04 23:57:18 +0900

    fix app.tsx.

--- src/app/javascript/source/App.tsx
+++ src/app/javascript/source/App.tsx
@@ -1,42 +1,20 @@
 import React from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
 import "ress";
 import "./App.css";
-
-const NavBar = styled.nav``;
-const Inner = styled.div`
-  margin: 0 auto;
-  padding: 15px 0;
-  display: flex;
-  justify-content: space-between;
-  width: 1000px;
-`;
-const Logo = styled.div``;
-const NavItems = styled.ul`
-  display: flex;
-`;
-const NavItem = styled.li`
-  padding: 0 10px;
-`;
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import EditTodo from "./components/EditTodo";
 
 const App = () => {
   return (
-    <>
-      <NavBar>
-        <Inner>
-          <Logo>TODO</Logo>
-          <NavItems>
-            <NavItem>
-              <Link to="/todos">Todos</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="/todos/new">新規投稿</Link>
-            </NavItem>
-          </NavItems>
-        </Inner>
-      </NavBar>
-    </>
+    <Router>
+      <Routes>
+        <Route path={"/todos"} element={<TodoList />} />
+        <Route path={"/todos/new"} element={<AddTodo />} />
+        <Route path={"/todos/:id/edit"} element={<EditTodo />} />
+      </Routes>
+    </Router>
   );
 };
 
