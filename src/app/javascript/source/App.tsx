513b5f8d16fc771bfcdc3050406c9c1b5f0f5464
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-04-05 00:40:29 +0900

    fix App.tsx

--- src/app/javascript/source/App.tsx
+++ src/app/javascript/source/App.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { FC } from "react";
 import "ress";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import EditTodo from "./components/EditTodo";
+import Layout from "./components/layout";
 
-const App = () => {
+const App: FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path={"/todos"} element={<TodoList />} />
-        <Route path={"/todos/new"} element={<AddTodo />} />
-        <Route path={"/todos/:id/edit"} element={<EditTodo />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path={"/todos"} element={<TodoList />} />
+          <Route path={"/todos/new"} element={<AddTodo />} />
+          <Route path={"/todos/:id/edit"} element={<EditTodo />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
