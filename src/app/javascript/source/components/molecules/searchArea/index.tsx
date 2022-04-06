79292ca4fc7dfb15f00b77dfe6f681a492678a04
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-04-06 23:39:26 +0900

    create searchArea component.

---
+++ src/app/javascript/source/components/molecules/searchArea/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "../../atoms/button";
+import { Main } from "./style";
+import Input from "../../atoms/input";
+
+const SearchArea = ({ setSearchName, removeAllTodos }) => {
+  return (
+    <Main>
+      <Input
+        placeholder="Search todo."
+        onChange={e => {
+          setSearchName(e.target.value);
+        }}
+      />
+      <Button onClick={removeAllTodos}>Remove All</Button>
+    </Main>
+  );
+};
+
+export default SearchArea;
---
+++ src/app/javascript/source/components/molecules/searchArea/style.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  margin-top: 10px;
+`;
+export const SearchInput = styled.input``;
