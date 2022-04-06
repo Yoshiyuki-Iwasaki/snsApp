68ab4c3d67abd41254258afd1ba18b5413052f64
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-04-07 00:27:38 +0900

    fix todoApp component.

--- src/app/javascript/source/components/organisms/todoApp/index.tsx
+++ src/app/javascript/source/components/organisms/todoApp/index.tsx
@@ -65,6 +65,15 @@ const TodoApp: FC = () => {
               return val;
             }
           })
+          .sort((a, b) => {
+            if (a.id < b.id) {
+              return 1;
+            }
+            if (a.id > b.id) {
+              return -1;
+            }
+            return 0;
+          })
           .map((val, index) => {
             return (
               <ListItem
--- src/app/javascript/source/components/organisms/todoApp/style.tsx
+++ src/app/javascript/source/components/organisms/todoApp/style.tsx
@@ -1,3 +1,5 @@
 import styled from "styled-components";
 
-export const List = styled.div``;
+export const List = styled.ul`
+  margin-top: 10px;
+`;
