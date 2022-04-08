8e4613015d79db760378946821bce6e837f69b52
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-04-08 20:08:10 +0900

    fix api file.

--- frontend/src/api/Todo/api.ts
+++ frontend/src/api/Todo/api.ts
@@ -1,15 +1,38 @@
 import api from "../api";
 
+const ENDPOINT = "/api/v1/todos";
+
 async function fetch() {
-  return api.get("/api/v1/todos");
+  return api.get(ENDPOINT);
 }
+
+async function fetch_detail(id: number) {
+  return api.get(ENDPOINT + `/` + id);
+}
+
 async function create(todo: any) {
-  return api.post("/api/v1/todos", todo);
+  return api.post(ENDPOINT, todo);
+}
+
+async function update(id: number, todo: any) {
+  return api.patch(ENDPOINT + `/` + id, todo);
+}
+
+async function remove(id: number) {
+  return api.delete(ENDPOINT + `/` + id);
+}
+
+async function remove_all(id: number) {
+  return api.delete(ENDPOINT + `/` + id);
 }
 
 const TodoApi = {
   fetch,
+  fetch_detail,
   create,
+  update,
+  remove,
+  remove_all,
 };
 
 export default TodoApi;
