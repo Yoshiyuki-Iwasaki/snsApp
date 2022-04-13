eeaa67d8528749b28cf0ecd45eb48f3e4275866e
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-04-13 13:03:51 +0900

    add user_id column.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_101008) do
+ActiveRecord::Schema.define(version: 2022_04_12_235907) do
 
   create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "completed", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
