2b31fd4c145989e7bfa083cfbfee91d703746972
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-04-28 12:39:14 +0900

    fix custom hooks.

--- frontend/src/hooks/useFetchAllTodos.ts
+++ frontend/src/hooks/useFetchAllTodos.ts
@@ -1,18 +1,29 @@
 import { useState, useEffect } from "react";
 import TodoApi from "../api/Todo/api";
 
-const useFetchAllTodos = pageNumber => {
-  const [todos, setTodos] = useState([]);
+const useFetchAllTodos = () => {
+  const [isFetching, setIsFetching] = useState(false);
+  const [hasMore, setHasMore] = useState(true); //再読み込み判定
+  const [todos, setTodos] = useState<any[]>([]);
 
-  useEffect(() => {
-    const fetchTodo = async () => {
-      const todoRes = await TodoApi.fetch(pageNumber);
-      setTodos(todoRes.data);
-    };
-    fetchTodo();
-  }, []);
+  const fetchTodo = async page => {
+    try {
+      setIsFetching(true);
+      {
+        const todoRes = await TodoApi.fetch(page);
+        //データ件数が0件の場合、処理終了
+        if (todoRes.data.length < 1) {
+          setHasMore(false);
+          return;
+        }
+        setTodos([...todos, ...todoRes.data]);
+      }
+    } finally {
+      setIsFetching(false);
+    }
+  };
 
-  return todos;
+  return { todos, fetchTodo, hasMore, isFetching };
 };
 
 export default useFetchAllTodos;
--- frontend/src/page/topPage/index.tsx
+++ frontend/src/page/topPage/index.tsx
@@ -1,34 +1,12 @@
-import React, { useState, useEffect, FC } from "react";
+import React, { useState, FC } from "react";
 import Presenter from "./presenter";
 import useFetchAllTodos from "../../hooks/useFetchAllTodos";
 import useFetchMyUser from "../../hooks/useFetchMyUser";
-import TodoApi from "../../api/Todo/api";
-
-let pageNumber: number = 1;
 
 const TopPage: FC = () => {
-  const [searchName, setSearchName] = useState("");
+  const [searchName, setSearchName] = useState<string>("");
   const { myUser } = useFetchMyUser();
-  const [todos, setTodos] = useState<any[]>([]);
-  const [hasMore, setHasMore] = useState(true); //再読み込み判定
-  const [isFetching, setIsFetching] = useState(false);
-
-  const fetchTodo = async page => {
-    try {
-      setIsFetching(true);
-      {
-        const todoRes = await TodoApi.fetch(page);
-        //データ件数が0件の場合、処理終了
-        if (todoRes.data.length < 1) {
-          setHasMore(false);
-          return;
-        }
-        setTodos([...todos, ...todoRes.data]);
-      }
-    } finally {
-      setIsFetching(false);
-    }
-  };
+  const { todos, fetchTodo, hasMore, isFetching } = useFetchAllTodos();
 
   return (
     <Presenter
