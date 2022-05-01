193b6cdffe9bdbc238b90471535a0fb13ad4b036
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-01 17:45:26 +0900

    fix layout.

--- frontend/src/components/organisms/header/presenter.tsx
+++ frontend/src/components/organisms/header/presenter.tsx
@@ -1,37 +1,35 @@
-import { Container, Button, Flex, Link, Text } from "@chakra-ui/react";
+import { Button, Flex, Link, Text } from "@chakra-ui/react";
 import React, { FC } from "react";
 import { PresenterType } from "./type";
 
 const Presenter: FC<PresenterType> = ({ myUser, handleLogout }) => {
   console.log("myUser", myUser);
   return (
-    <Container my={3}>
-      <Flex alignItems={"center"} justifyContent={"space-between"}>
-        <Link href="/">
-          <Text>TODO LIST</Text>
-        </Link>
-        <Flex alignItems={"center"}>
-          {myUser && myUser.isLogin ? (
-            <>
-              <Link mr={3} href={`/new`}>
-                新規投稿
-              </Link>
-              <Link mr={3} href={`/user/${myUser.data.id}`}>
-                {myUser.data.name}
-              </Link>
-              <Button onClick={handleLogout}>ログアウト</Button>
-            </>
-          ) : (
-            <>
-              <Link mr={3} href="/">
-                ログイン
-              </Link>
-              <Link href="/signup">新規登録</Link>
-            </>
-          )}
-        </Flex>
+    <Flex px={5} py={3} alignItems={"center"} justifyContent={"space-between"}>
+      <Link href="/">
+        <Text>TODO LIST</Text>
+      </Link>
+      <Flex alignItems={"center"}>
+        {myUser && myUser.isLogin ? (
+          <>
+            <Link mr={3} href={`/new`}>
+              新規投稿
+            </Link>
+            <Link mr={3} href={`/user/${myUser.data.id}`}>
+              {myUser.data.name}
+            </Link>
+            <Button onClick={handleLogout}>ログアウト</Button>
+          </>
+        ) : (
+          <>
+            <Link mr={3} href="/">
+              ログイン
+            </Link>
+            <Link href="/signup">新規登録</Link>
+          </>
+        )}
       </Flex>
-    </Container>
+    </Flex>
   );
 };
 
