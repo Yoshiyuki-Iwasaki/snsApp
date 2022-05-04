97bfcc1fa66a9fde81c7fe8bb473cb9c2e13a326
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-04 18:29:31 +0900

    create notification page.

---
+++ frontend/src/page/notificationPage/presenter.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { Box, Link, Button } from '@chakra-ui/react';
+import { PresenterType } from './type';
+
+const Presenter: FC<PresenterType> = () => {
+  return (
+    <>
+      <Box>通知ページ</Box>
+    </>
+  );
+};
+
+export default Presenter;
