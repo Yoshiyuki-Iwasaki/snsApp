9e9137cf6707bbb70c882acec6634fd538659e3e
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-06 10:51:13 +0900

    create to edit profile page.

---
+++ frontend/src/page/editProfilePage/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import Presenter from './presenter';
+
+const EditProfilePage: FC = () => {
+  return <Presenter />;
+};
+
+export default EditProfilePage;
---
+++ frontend/src/page/editProfilePage/presenter.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { PresenterType } from './type';
+import { Box, Center, FormLabel, Input, Button } from '@chakra-ui/react';
+
+const Presenter: FC = ({}) => {
+  return (
+    <Box p="5" w="500px" borderWidth="1px">
+      EditProfilePage
+    </Box>
+  );
+};
+
+export default Presenter;
