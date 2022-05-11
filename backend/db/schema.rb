89044cd4288a64bfc8c2c685b7b74bfafb0d282d
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-11 23:28:57 +0900

    create chats table.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_08_111931) do
+ActiveRecord::Schema.define(version: 2022_05_11_141946) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2022_05_08_111931) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "chats", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "poster_id", null: false
+    t.bigint "posted_id", null: false
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["posted_id"], name: "index_chats_on_posted_id"
+    t.index ["poster_id"], name: "index_chats_on_poster_id"
   end
 
   create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -118,6 +128,8 @@ ActiveRecord::Schema.define(version: 2022_05_08_111931) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "chats", "users", column: "posted_id"
+  add_foreign_key "chats", "users", column: "poster_id"
   add_foreign_key "favorites", "posts"
   add_foreign_key "favorites", "replies"
   add_foreign_key "favorites", "users"
