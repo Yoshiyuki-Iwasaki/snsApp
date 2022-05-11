d59c10520040c6c9ec28a3bb1364f34fc46e7441
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-11 09:16:20 +0900

    fix hooks.

--- frontend/src/hooks/useFetchMyPost.ts
+++ frontend/src/hooks/useFetchMyPost.ts
@@ -1,17 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import PostApi from '../api/Post/api';
 
 // 自分が投稿したPostを取得するcustom hooks.
 const useFetchMyPost = (id: number) => {
   const [myPost, setMyPost] = useState<any>();
+  const fetchMyPost = useCallback(async () => {
+    const PostRes = await PostApi.fetch_userPost(id);
+    setMyPost(PostRes.data);
+  }, [id]);
 
   useEffect(() => {
-    const fetchMyPost = async () => {
-      const PostRes = await PostApi.fetch_userPost(id);
-      setMyPost(PostRes.data);
-    };
     fetchMyPost();
-  }, []);
+  }, [id, fetchMyPost]);
 
   return myPost;
 };
