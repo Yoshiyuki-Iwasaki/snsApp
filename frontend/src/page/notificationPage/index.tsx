dd94b0ccd0bf5ddbce2057fb746b218a9923516d
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-16 06:32:21 +0900

    create useHandleNotificationCheck hooks.

--- frontend/src/page/notificationPage/index.tsx
+++ frontend/src/page/notificationPage/index.tsx
@@ -2,26 +2,11 @@ import React, { FC } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import Presenter from './presenter';
 import useFetchNotification from '../../hooks/useFetchNotification';
-import { useNavigate } from 'react-router-dom';
-import NotificationApi from '../../api/Notification/api';
+import useHandleNotificationCheck from '../../hooks/useHandleNotificationCheck';
 
 const NotificationPage: FC = () => {
   const { notification } = useFetchNotification();
-  const navigate = useNavigate();
-
-  const handleNotificationCheck = async (val) => {
-    const data = {
-      checked: true,
-    };
-    await NotificationApi.update(val.id, data);
-    if (val.type === 'like' || val.type === 'reply') {
-      navigate('/post/' + val.post.id);
-    } else if (val.type === 'follow') {
-      navigate('/user/' + val.visiter.id);
-    } else if (val.type === 'chat') {
-      navigate('/chatRoom/' + val.url);
-    }
-  };
+  const handleNotificationCheck = useHandleNotificationCheck();
 
   return (
     <Presenter
