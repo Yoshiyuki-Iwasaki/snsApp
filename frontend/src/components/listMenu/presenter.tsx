70168c2694905145082e8f40bd9e9e88c131bc8c
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-16 09:06:17 +0900

    fix listMenu component.

--- frontend/src/components/listMenu/presenter.tsx
+++ frontend/src/components/listMenu/presenter.tsx
@@ -14,52 +14,56 @@ const Presenter: FC<ListMenuType> = ({
 }) => {
   return (
     <>
-      {myUser && myUser.data.id === postedUser.id && (
-        <Button
-          m={0}
-          p={0}
-          w={30}
-          h={30}
-          minWidth={0}
-          minHeight={0}
-          lineHeight={1}
-          position="absolute"
-          top={2}
-          right={3}
-          onClick={handleModal}
-        >
-          <HamburgerIcon />
-        </Button>
-      )}
-      {modalOpenFrag && (
-        <Box position="absolute" top={2} right={16}>
-          {!replyFrag && (
-            <Box>
-              <Link
-                href={'/post/' + val.id + '/edit'}
-                fontSize="13px"
-                _hover={{ textDecoration: 'none' }}
-              >
-                編集
-              </Link>
-            </Box>
-          )}
-          <Box>
+      {postedUser && myUser && (
+        <>
+          {myUser.data.id === postedUser.id && (
             <Button
               m={0}
               p={0}
+              w={30}
+              h={30}
               minWidth={0}
-              height="auto"
-              width="auto"
-              bgColor="#fff"
-              fontSize="13px"
-              fontWeight={400}
-              onClick={deletePost}
+              minHeight={0}
+              lineHeight={1}
+              position="absolute"
+              top={2}
+              right={3}
+              onClick={handleModal}
             >
-              削除
+              <HamburgerIcon />
             </Button>
-          </Box>
-        </Box>
+          )}
+          {modalOpenFrag && (
+            <Box position="absolute" top={2} right={16}>
+              {!replyFrag && (
+                <Box>
+                  <Link
+                    href={'/post/' + val.id + '/edit'}
+                    fontSize="13px"
+                    _hover={{ textDecoration: 'none' }}
+                  >
+                    編集
+                  </Link>
+                </Box>
+              )}
+              <Box>
+                <Button
+                  m={0}
+                  p={0}
+                  minWidth={0}
+                  height="auto"
+                  width="auto"
+                  bgColor="#fff"
+                  fontSize="13px"
+                  fontWeight={400}
+                  onClick={deletePost}
+                >
+                  削除
+                </Button>
+              </Box>
+            </Box>
+          )}
+        </>
       )}
     </>
   );
