970f73dc26dc620569db6fbf7c9dc6ee3b11ef8b
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-19 05:15:33 +0900

    create error page.

---
+++ frontend/src/page/errorPage/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import 'react-toastify/dist/ReactToastify.css';
+import Presenter from './presenter';
+
+const ErrorPage: FC = () => {
+  return <Presenter />;
+};
+
+export default ErrorPage;
---
+++ frontend/src/page/errorPage/presenter.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const Presenter: FC = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>ページが見つかりません</p>
+    </div>
+  );
+};
+
+export default Presenter;
