a4dc271c8cda7a5a05b17c0c4f4421ec3d7b2890
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-29 22:24:06 +0900

    add comment text.

--- frontend/src/components/searchArea/presenter.test.tsx
+++ frontend/src/components/searchArea/presenter.test.tsx
@@ -3,36 +3,47 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Presenter from './presenter';
 
-const searchText = {
+// ダミーデータ
+const dummySearchText = {
   content: 'test',
 };
 
 describe('Input', () => {
   it('renders correctly', () => {
+    // レンダリング
     render(
       <Presenter
-        search={searchText}
+        search={dummySearchText}
         handleInputChange={() => {}}
         handleSearch={() => {}}
       />
     );
+
+    // 検索ボタン
     const search_button: any = screen.getByTestId('button');
+    // 検索フォーム
     const search_input: any = screen.getByTestId('input');
 
+    // 検索ボタンがレンダリングされていることを確認
     expect(search_button).toBeTruthy();
+    // 検索フォームがレンダリングされていることを確認
     expect(search_input).toBeTruthy();
   });
 
   it('update on change', () => {
+    // レンダリング
     render(
       <Presenter
-        search={test}
+        search={dummySearchText}
         handleInputChange={() => {}}
         handleSearch={() => {}}
       />
     );
+    // 検索フォーム
     const search_input: any = screen.getByTestId('input');
+    // 検索フォームにtestの値を入れる
     userEvent.type(search_input, 'test');
+    // 検索フォームのvalue値がtestか確認する
     expect(search_input.value).toBe('test');
   });
 });
