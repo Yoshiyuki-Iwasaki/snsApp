f74f217882b9f325055205dad924136de652915c
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-05-29 18:10:43 +0900

    remove unnecessary line.

--- frontend/src/page/chatRoomPage/type.ts
+++ frontend/src/page/chatRoomPage/type.ts
@@ -1,23 +1,5 @@
-import { PostsType } from '../../type/Posts';
 import { MyUserType } from '../../type/MyUser';
-import { deletePostType } from '../../type';
-import { handleModalType } from '../../type';
 import { handleInputChangeType } from '../../type';
-import { addReplyType } from '../../type';
-
-export type DetailPageType = {
-  currentPost: PostsType;
-  myUser: MyUserType;
-  reply: {
-    content: string;
-  };
-  replies: PostsType[];
-  modalOpenFrag: boolean;
-  deletePost: deletePostType;
-  handleModal: handleModalType;
-  handleInputChange: handleInputChangeType;
-  addReply: addReplyType;
-};
 
 export type PresenterType = {
   myUser: MyUserType;
